1a815c7201b5b34006487234ed5a3416b4670671
Tudor <sekaru@sleepystudios.net>
2017-07-12 19:37:06 +0100

    Waker functionality

--- core/src/net/sleepystudios/ld38/LD38.java
+++ core/src/net/sleepystudios/ld38/LD38.java
@@ -15,15 +15,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import net.sleepystudios.ld38.particles.ParticleEffect;
 import net.sleepystudios.ld38.particles.Action;
+import net.sleepystudios.waker.Waker;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -70,7 +68,7 @@ public class LD38 extends ApplicationAdapter implements ActionListener, InputPro
         Music music = Gdx.audio.newMusic(Gdx.files.internal("vibrations.mp3"));
         music.setVolume(0.8f);
         music.setLooping(true);
-        music.play();
+        //music.play();
 
         Gdx.input.setInputProcessor(this);
 	}
@@ -84,21 +82,22 @@ public class LD38 extends ApplicationAdapter implements ActionListener, InputPro
                     // ip
                     if (line.startsWith("ip")) {
                         if (line.split(":")[1].contains("auto")) {
-                            ip = getMasterIP();
+                            String[] server = getMasterIP().split(":");
+
+                            ip = server[0];
+                            tcp = Integer.valueOf(server[1]);
+                            udp = Integer.valueOf(server[2]);
+                            return;
                         } else {
                             ip = line.split(":")[1];
                         }
                     }
 
                     // tcp
-                    if (line.startsWith("tcp")) {
-                        tcp = Integer.valueOf(line.split(":")[1]);
-                    }
+                    if (line.startsWith("tcp")) tcp = Integer.valueOf(line.split(":")[1]);
 
                     // udp
-                    if (line.startsWith("udp")) {
-                        udp = Integer.valueOf(line.split(":")[1]);
-                    }
+                    if (line.startsWith("udp")) udp = Integer.valueOf(line.split(":")[1]);
                 }
             }
         } finally {
@@ -107,13 +106,8 @@ public class LD38 extends ApplicationAdapter implements ActionListener, InputPro
     }
 
     public String getMasterIP() throws IOException {
-        String url = "http://sleepystudios.net/entropyip.txt";
-        URL urlObj = new URL(url);
-        URLConnection lu = urlObj.openConnection();
-
-        // get the response
-        BufferedReader rd = new BufferedReader(new InputStreamReader(lu.getInputStream()));
-        return rd.readLine();
+	    Waker waker = new Waker("entropy");
+	    return waker.getServer();
     }
 
     // generates a random number
